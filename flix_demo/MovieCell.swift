280ec093a9346360ba9912350e0cc07673c2454c
Kiya Segni <kiyasegni@Kiyas-MacBook-Air.local>
2018-10-01 20:58:42 -0400

    added tab bar

---
+++ flix_demo/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix_demo
+//
+//  Created by Kiya Segni on 9/14/18.
+//  Copyright © 2018 Kiya Segni. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
